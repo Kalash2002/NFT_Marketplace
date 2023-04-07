259000e84ace1f2c218b0dbf23480e4b6ba24814
Kalash2002 <202051097@iiitvadodara.ac.in>
2023-04-08 00:00:24 +0530

    upto collection

--- frontend/Components/componentIndex.js
+++ frontend/Components/componentIndex.js
@@ -7,5 +7,8 @@ import BigNFTSlider from "./BigNFTSlider/BigNFTSlider";
 import Subscribe from "./Subscribe/Subscribe";
 import Title from "./Title/Title";
 import Category from "./Category/Category";
+import Filter from "./Filter/Filter";
+import NFTCard from "./NFTCard/NFTCard";
+import Collection from "./Collection/Collection";
 
-export {Button,Footer,Navbar,HeroSection,Services,BigNFTSlider,Subscribe,Title,Category};
+export {Button,Footer,Navbar,HeroSection,Services,BigNFTSlider,Subscribe,Title,Category,Filter,NFTCard,Collection};
--- frontend/src/pages/index.js
+++ frontend/src/pages/index.js
@@ -8,6 +8,9 @@ import {
   Subscribe,
   Title,
   Category,
+  Filter,
+  NFTCard,
+  Collection
 } from "../../Components/componentIndex";
 
 const Home = () => {
@@ -17,10 +20,21 @@ const Home = () => {
       <Services />
       <BigNFTSlider />
       <Title
+        heading="Filter By Collection"
+        paragraph="Discover the most outstanding NFTs in all topics of life."
+      />
+      <Collection/>
+      <Title
+        heading="Featured NFT's"
+        paragraph="Discover the most outstanding NFTs in all topics of life."
+      />
+      <Filter />
+      <NFTCard/>
+      <Title
         heading="Audio Collection"
         paragraph="Discover the most outstanding NFTs in all topics of life."
       />
-      <Category/>
+      <Category />
       <Subscribe />
     </div>
   );

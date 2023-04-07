315eb9aded71c49e9b62039c4257354028a3b2a5
Kalash2002 <202051097@iiitvadodara.ac.in>
2023-04-07 16:52:39 +0530

    upto category

--- frontend/Components/componentIndex.js
+++ frontend/Components/componentIndex.js
@@ -4,5 +4,8 @@ import Navbar from "./Navbar/Navbar";
 import HeroSection from "./HeroSection/HeroSection";
 import Services from "./Services/Services";
 import BigNFTSlider from "./BigNFTSlider/BigNFTSlider";
+import Subscribe from "./Subscribe/Subscribe";
+import Title from "./Title/Title";
+import Category from "./Category/Category";
 
-export {Button,Footer,Navbar,HeroSection,Services,BigNFTSlider};
+export {Button,Footer,Navbar,HeroSection,Services,BigNFTSlider,Subscribe,Title,Category};
--- frontend/src/pages/index.js
+++ frontend/src/pages/index.js
@@ -1,16 +1,29 @@
-import React from 'react'
+import React from "react";
 
-import Styles from'../styles/index.module.css';
-import { HeroSection,Services,BigNFTSlider } from '../../Components/componentIndex';
+import Styles from "../styles/index.module.css";
+import {
+  HeroSection,
+  Services,
+  BigNFTSlider,
+  Subscribe,
+  Title,
+  Category,
+} from "../../Components/componentIndex";
 
 const Home = () => {
   return (
     <div className={Styles.homePage}>
-        <HeroSection/>
-        <Services/>
-        <BigNFTSlider/>
+      <HeroSection />
+      <Services />
+      <BigNFTSlider />
+      <Title
+        heading="Audio Collection"
+        paragraph="Discover the most outstanding NFTs in all topics of life."
+      />
+      <Category/>
+      <Subscribe />
     </div>
-  )
-}
+  );
+};
 
 export default Home;
